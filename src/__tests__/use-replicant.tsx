8ab272e16aab41d2d0f13b9aa1d619f23156662d
Hoishin <hoishinxii@gmail.com>
2019-03-04 03:35:43 +0900

    style(lint): fix for eslint

--- src/__tests__/use-replicant.tsx
+++ src/__tests__/use-replicant.tsx
@@ -1,7 +1,7 @@
-// tslint:disable no-any
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
 
 import {EventEmitter} from 'events';
-import * as React from 'react';
+import React from 'react';
 import {render, RenderResult} from 'react-testing-library';
 import {useReplicant} from '..';
 
@@ -10,11 +10,11 @@ const replicantRemoveListener = jest.fn();
 
 // Intercept mock function
 class Replicant extends EventEmitter {
-	on(event: string, payload: any) {
+	on(event: string, payload: any): this {
 		replicantHandler(event, payload);
 		return super.on(event, payload);
 	}
-	removeListener(event: string, listener: () => void) {
+	removeListener(event: string, listener: () => void): this {
 		replicantRemoveListener();
 		return super.removeListener(event, listener);
 	}
@@ -27,7 +27,7 @@ const replicantConstructor = jest.fn(() => replicant);
 	Replicant: replicantConstructor,
 };
 
-const RunnerName = () => {
+const RunnerName = (): JSX.Element => {
 	const [currentRun] = useReplicant('currentRun', {runner: {name: 'foo'}});
 	return <div>{currentRun.runner.name}</div>;
 };
@@ -49,7 +49,7 @@ test('Change handler is set correctly', () => {
 	expect(replicantHandler).toBeCalledTimes(1);
 });
 
-test.skip('Handles replicant changes', async () => {
+test.skip('Handles replicant changes', () => {
 	replicant.emit('change', {runner: {name: 'bar'}});
 	renderResult.rerender(<RunnerName />);
 	expect(renderResult.container.textContent).toBe('bar');
