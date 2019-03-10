b009f1fe9c497bbdcc6db217507c0f82e8719f10
Ted Kulp <ted@tedkulp.com>
2019-03-10 12:47:14 -0400

    Add options objects so we can have namespace/bundle support
    Fixes #1

--- src/useReplicant/index.ts
+++ src/useReplicant/index.ts
@@ -1,24 +1,37 @@
 import {useEffect, useState} from 'react';
 import clone from 'lodash.clone';
 
+export interface UseReplicantOptions {
+	namespace: string;
+}
+
 /**
  * Subscribe to a replicant, returns tuple of the replicant value and `setValue` function.
  * The component using this function gets re-rendered when the value is updated.
  * The `setValue` function can be used to update replicant value.
  * @param repName The name of the replicant to use
  * @param initialValue Initial value to pass to `useState` function
+ * @param options Options object.  Currently supports the optional `namespace` option
  */
 export const useReplicant = <T>(
 	repName: string,
 	initialValue: T,
+	options?: UseReplicantOptions,
 ): [T, (newValue: T) => void] => {
 	// Local state to store replicant value
 	const [value, updateValue] = useState<T>(initialValue);
 
 	// Declare replicant
-	const replicant = nodecg.Replicant(repName, {
-		defaultValue: initialValue,
-	});
+	let replicant: any;
+	if (options && options.namespace) {
+		replicant = nodecg.Replicant(repName, options.namespace, {
+			defaultValue: initialValue,
+		});
+	} else {
+		replicant = nodecg.Replicant(repName, {
+			defaultValue: initialValue,
+		});
+	}
 
 	// Change handler to listen replicant changes
 	const changeHandler = (newValue: T): void => {
