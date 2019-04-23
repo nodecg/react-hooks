80805fb30d9fa3bd9a518402a35385fa41884078
Ted Kulp <github@wishy.org>
2019-04-22 21:06:26 -0400

    Add options objects so we can have namespace/bundle support (#2)
    Fixes #1

--- src/useReplicantOnce/index.ts
+++ src/useReplicantOnce/index.ts
@@ -1,12 +1,23 @@
 import {useState} from 'react';
 
+export interface UseReplicantOnceOptions {
+	bundle: string;
+}
+
 export const useReplicantOnce = <T>(
 	replicantName: string,
 	initialValue: T,
+	options?: UseReplicantOnceOptions,
 ): T => {
 	const [state, setState] = useState(initialValue);
-	nodecg.readReplicant<T>(replicantName, (value) => {
-		setState(value);
-	});
+	if (options && options.bundle) {
+		nodecg.readReplicant<T>(replicantName, options.bundle, (value) => {
+			setState(value);
+		});
+	} else {
+		nodecg.readReplicant<T>(replicantName, (value) => {
+			setState(value);
+		});
+	}
 	return state;
 };
