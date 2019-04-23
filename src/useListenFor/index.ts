cca0df8ed79e9cd34309963242403181af4f2223
Ted Kulp <github@wishy.org>
2019-04-22 21:06:26 -0400

    feat(useReplicant): allow specifying namespace (#2)
    Fixes #1

--- src/useListenFor/index.ts
+++ src/useListenFor/index.ts
@@ -1,10 +1,21 @@
 import {useEffect} from 'react';
 
+export interface UseListenForOptions {
+	bundle: string;
+}
+
 export const useListenFor = <T>(
 	messageName: string,
 	handler: (message: T) => void,
+	options?: UseListenForOptions,
 ): void => {
 	useEffect(() => {
+		if (options && options.bundle) {
+			nodecg.listenFor(messageName, options.bundle, handler);
+			return () => {
+				nodecg.unlisten(messageName, options.bundle, handler);
+			};
+		}
 		nodecg.listenFor(messageName, handler);
 		return () => {
 			nodecg.unlisten(messageName, handler);
